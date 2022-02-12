f126fd056a4aaa64e77b1ef8a1f1e16761e2e5d1
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-12 17:09:42 +0530

    Create __main__.py

---
+++ userbot/__main__.py
@@ -0,0 +1,101 @@
+import glob
+import logging
+import os
+from pathlib import Path
+from sys import argv
+
+import telethon.utils
+from telethon import TelegramClient
+
+from userbot import CMD_HNDLR, bot
+from userbot.Config import Var
+from userbot.thunderconfig import Config
+from userbot.utils import load_assistant, load_module, start_assistant
+
+TELE = Var.PRIVATE_GROUP_ID
+BOTNAME = Var.TG_BOT_USER_NAME_BF_HER
+LOAD_MYBOT = Var.LOAD_MYBOT
+sed = logging.getLogger("JOCASTA")
+
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    bot.me = await bot.get_me()
+    bot.uid = telethon.utils.get_peer_id(bot.me)
+
+
+async def startup_log_all_done():
+    try:
+        await bot.send_message(
+            TELE,
+            f"**JOCASTA has been deployed.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\nAdd** @{BOTNAME} **to this group and make it admin for enabling all the features of userbot**",
+        )
+    except BaseException:
+        print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
+
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
+        bot.tgbot = TelegramClient(
+            "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
+        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished, no errors")
+        print("Starting Userbot")
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
+    else:
+        bot.start()
+
+path = "userbot/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        try:
+            
+         load_module(shortname.replace(".py", ""))
+        except Exception:
+            pass
+print("Jocasta has been deployed! ")
+
+print("Setting up JOCASTA")
+
+
+if Config.ENABLE_ASSISTANTBOT == "ENABLE":
+    path = "userbot/plugins/assistant/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            try:
+                
+            
+             load_assistant(shortname.replace(".py", ""))
+            except Exception:
+                pass
+    sed.info("Jocasta Has Been Deployed Successfully !")
+    sed.info("╔════❰ 𝑩𝑶𝑻 𝑰𝑵𝑭𝑶𝑹𝑴𝑨𝑻𝑰𝑶𝑵 ❱═❍⊱❁۪۪")
+    sed.info("║┣⪼ 𝑂𝑊𝑁𝐸𝑅 - 𝐽𝑂𝐶𝐴𝑆𝑇𝐴 𝑈𝑆𝐸𝑅 ")
+    sed.info("║┣⪼ 𝑆𝑇𝐴𝑇𝑈𝑆 - 𝑂𝑁𝐿𝐼𝑁𝐸")
+    sed.info("║┣⪼ 𝐵𝑂𝑇 𝑉𝐸𝑅𝑆𝐼𝑂𝑁 - 1.2.0")   
+    sed.info("║┣⪼ 𝑈𝑃𝑇𝐼𝑀𝐸 - 00h:00m:4s ")
+    sed.info("║┣⪼ 𝐵𝑂𝑇 𝑃𝐼𝑁𝐺 - 0.006")
+    sed.info("║┣⪼ 𝑃𝑌𝑇𝐻𝑂𝑁 - 3.9.2")
+    sed.info("║┣⪼ 𝑇𝐸𝐿𝐸𝑇𝐻𝑂𝑁 - 1.17.0 ")
+    sed.info("║┣⪼ ✨𝐉𝐎𝐂𝐀𝐒𝐓𝐀 𝐔𝐒𝐄𝐑𝐁𝐎𝐓✨")
+    sed.info("║╰━━━━━━━━━━━━━━━➣ ")
+    sed.info("╚══════════════════❍⊱❁۪۪")
+else:
+    sed.info("Jocasta Has Been Installed Sucessfully !")
+    sed.info("You Can Visit @JOCASTA_SUPPORT For Any Support Or Doubts")
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()  
