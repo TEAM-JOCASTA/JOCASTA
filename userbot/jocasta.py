95d71affc9fd1f80b4569cec95e66c0fed04a814
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-12 17:15:03 +0530

    Create jocasta.py

---
+++ userbot/jocasta.py
@@ -0,0 +1,15 @@
+#if you read only this then okay else
+import os
+from userbot import ALIVE_NAME 
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "JOCASTA"
+NAME = os.environ.get("BOT_NICK_NAME")
+BOT =  = str(NAME) if NAME else "JOCASTA"
+#op for SUKHPAL443
+#op for TEAM-JOCASTA
+
+SUKHPAL443 = "[BLACK MAMBA](https://t.me/SUKHPAL_KHERERA)"
+OP = "[JOCASTA](https://github.com/TEAM-JOCASTA/JOCASTA)"
+OKAY = "[SUPPORT GROUP](https://t.me/JOCASTA_SUPPORT)" 
+ALIVE = "JOCASTA BOT IS ON 🔥 FIRE 🔥" #make by SUKHPAL KHERERA for JOCASTA
+USERBOT = " HELLO MASTER MY NAME IS JOCASTA BOT I AM A BEST USERBOT 💝"
+EMOJI = "⚡"
