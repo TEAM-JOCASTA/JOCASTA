3406b9b3766f86b6c5c4deee44eac53b2ce7043b
Jatindalal875 <79321850+Jatindalal875@users.noreply.github.com>
2022-02-14 11:44:53 +0530

    initial commit

---
+++ userbot/db/broadcast_db.py
@@ -0,0 +1,26 @@
+from .. import udB
+
+
+def get_channels():  # Returns List
+    return udB.get_key("BROADCAST") or []
+
+
+def is_channel_added(id_):
+    return id_ in get_channels()
+
+
+def add_channel(id_):
+    channels = get_channels()
+    if id_ not in channels:
+        channels.append(id_)
+        udB.set_key("BROADCAST", channels)
+    return True
+
+
+def rem_channel(id_):
+    channels = get_channels()
+    if id_ in channels:
+        channels.remove(id_)
+        udB.set_key("BROADCAST", channels)
+        return True
+    return False
