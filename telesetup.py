697746e7c28fc54a8bd5b0811271311f51c05f2b
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-15 21:18:41 +0530

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -1,55 +1,10 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
 # Error Fixed by @krish1303y
 #!/usr/bin/env python3
 # This Source Code Form is subject to the terms of the GNU
 # General Public License, v.3.0. If a copy of the GPL was not distributed with this
 # file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
-print(""" ──╔╦═══╦═══╦═══╦═══╦════╦═══╗
+print("""
+ ──╔╦═══╦═══╦═══╦═══╦════╦═══╗
  ──║║╔═╗║╔═╗║╔═╗║╔═╗║╔╗╔╗║╔═╗║
  ──║║║─║║║─╚╣║─║║╚══╬╝║║╚╣║─║║
  ╔╗║║║─║║║─╔╣╚═╝╠══╗║─║║─║╚═╝║
