f93e0b0b985c7c3fa020205d5b1ae664ec32cc17
UNKNOWN <79321850+Jatindalal875@users.noreply.github.com>
2022-10-13 13:53:19 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -93,7 +93,7 @@ if Config.ENABLE_ASSISTANTBOT == "ENABLE":
     sed.info("╚══════════════════❍⊱❁۪۪")
 else:
     sed.info("Jocasta Has Been Installed Sucessfully !")
-    sed.info("You Can Visit @JOCASTA_SUPPORT For Any Support Or Doubts")
+    sed.info("You Can Visit @TEAM_BLACK_SUPPORT For Any Support Or Doubts")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
